b0815e02697516e682df42c668f161a7b76e9495
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-12 17:08:02 -0300

    feat: creating user repository Interface

---
+++ src/api/interfaces/IUserRepository.ts
@@ -0,0 +1,9 @@
+import type UserEntity from '../../entities/UserEntity'
+
+export interface IUserrepository {
+  create: (user: UserEntity) => Promise<void>
+  getUser: (id: string) => Promise<UserEntity | null>
+  getAll: () => Promise<UserEntity[] | null>
+  update: (id: string) => Promise<UserEntity | null>
+  delete: (id: string) => Promise<UserEntity | null>
+}
