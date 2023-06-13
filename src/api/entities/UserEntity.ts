bf6057d2ef781f290dd4d3f8555079cadd548ac2
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-13 16:50:01 -0300

    refactor: modified id atribuition

--- src/api/entities/UserEntity.ts
+++ src/api/entities/UserEntity.ts
@@ -1,20 +1,15 @@
 import { uuid } from 'uuidv4'
 
 class UserEntity {
-  public readonly id: string | undefined
+  public readonly id: string
 
   public name: string
   public email: string
-  public password: string
 
-  constructor (data: Omit<UserEntity, 'id'>, id?: string) {
+  constructor (data: UserEntity) {
     this.name = data.name
     this.email = data.email
-    this.password = data.password
-
-    if (id === null || id === undefined) {
-      this.id = uuid()
-    }
+    this.id = uuid()
   }
 }
 
