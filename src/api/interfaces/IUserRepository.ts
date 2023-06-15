992171b0387db1e9c46218bbf9eba9ab8edd4660
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-15 18:57:01 -0300

    refactor: modified params on method

--- src/api/interfaces/IUserRepository.ts
+++ src/api/interfaces/IUserRepository.ts
@@ -1,9 +1,9 @@
 import type UserEntity from '../entities/UserEntity'
 
-export interface IUserrepository {
+export interface IUserRepository {
   create: (user: UserEntity) => Promise<void>
   getUser: (id: string) => Promise<UserEntity | null>
   getAll: () => Promise<UserEntity[] | null>
-  update: (id: string) => Promise<UserEntity | null>
+  update: (body: UserEntity) => Promise<UserEntity | null>
   delete: (id: string) => Promise<UserEntity | null>
 }
