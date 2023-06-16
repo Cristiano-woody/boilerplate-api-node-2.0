b55e8d2173a9856327af0b24d65e79dc6cf5de21
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-16 11:52:44 -0300

    refactor: message init

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
+const portServer = process.env.PORT_API
+
 app.listen(process.env.PORT_API, () => {
-  console.log('aplicaćão iniciada')
+  console.log(`\n Aplicação iniciada na porta: ${portServer as string} `)
 })
