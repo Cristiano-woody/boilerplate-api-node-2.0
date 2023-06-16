1950c142a02d337a41897a718fca7768fba2b357
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-16 13:49:51 -0300

    feat: creating MokUserRepository

---
+++ src/api/repositories/MokUserRepository.ts
@@ -0,0 +1,40 @@
+
+import type UserEntity from '../entities/UserEntity'
+import { type IUserRepository } from '../interfaces/IUserRepository'
+import { PrismaClient } from '@prisma/client'
+
+class MokUserRepository implements IUserRepository {
+  private readonly prisma: PrismaClient
+
+  constructor () {
+    this.prisma = new PrismaClient()
+  }
+
+  async create (user: UserEntity): Promise<void> {
+    console.log('creating user')
+  }
+
+  async getAll (): Promise<UserEntity[] | null> {
+    const AllUsers = [{ name: 'user1', email: 'user@gmail.com', id: 'www' }, { name: 'user1', email: 'user@gmail.com', id: 'www' }]
+    return AllUsers
+  }
+
+  async getUser (id: string): Promise<UserEntity | null> {
+    const User = { name: 'user1', email: 'user@gmail.com', id: 'www' }
+    return User
+  }
+
+  async update (body: UserEntity): Promise<UserEntity | null> {
+    const User = { name: 'user1', email: 'user@gmail.com', id: 'www' }
+    console.log('atualizou')
+    return User
+  }
+
+  async delete (id: string): Promise<UserEntity | null> {
+    const User = { name: 'user1', email: 'user@gmail.com', id: 'www' }
+    console.log('deletou')
+    return User
+  }
+}
+
+export default MokUserRepository
