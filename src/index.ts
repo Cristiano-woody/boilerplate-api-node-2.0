7a30a724183ba907c93fd8cee82878b44bedceaa
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-16 13:40:59 -0300

    refactor: import MokRepository

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import UserRepository from './api/repositories/UserRepository'
 import UserService from './api/services/UserService'
 import UserController from './api/controllers/UserController'
 import UserRoutes from './api/routes/UserRoutes'
+// import MokUserRepository from './api/repositories/MokUserRepository'
 
 // instanciando as classes e injetando dependencias
 const CreateUserRepository = new UserRepository()
+// const CreateMokUserRepository = new MokUserRepository()
 const CreateUserService = new UserService(CreateUserRepository)
 const CreateUserController = new UserController(CreateUserService)
 const CreateUserRoutes = new UserRoutes(CreateUserController)
