2c326540191f4096aba7114ccf4cbb4dff0d0b87
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-16 10:46:16 -0300

    refactor: adding methods in UserController

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -11,9 +11,46 @@ class UserController implements IUserController {
 
   async create (req: Request, res: Response): Promise<Response> {
     try {
-    
+      await this.userService.create(req.body.user)
+      return res.status(201).send('user created successfully')
     } catch (err) {
+      return res.status(400).send(err)
+    }
+  }
 
+  async getUser (req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await this.userService.getUser(req.params.id)
+      return res.status(200).json(user)
+    } catch (err) {
+      return res.status(400).send(err)
+    }
+  }
+
+  async getAll (req: Request, res: Response): Promise<Response> {
+    try {
+      const allUsers = await this.userService.getAll()
+      return res.status(200).json(allUsers)
+    } catch (err) {
+      return res.status(400).send(err)
+    }
+  }
+
+  async update (req: Request, res: Response): Promise<Response> {
+    try {
+      await this.userService.update(req.params.id, req.body.user)
+      return res.status(200).send('user updated successfully')
+    } catch (err) {
+      return res.status(400).send(err)
+    }
+  }
+
+  async delete (req: Request, res: Response): Promise<Response> {
+    try {
+      await this.userService.delete(req.params.id)
+      return res.status(200).send('user deleted successfully')
+    } catch (err) {
+      return res.status(400).json(err)
     }
   }
 }
