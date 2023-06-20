aa74cd51cb79a9108bbc05a1d946095321918164
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-20 16:34:34 -0300

    refactor: Modified  interface Name

--- src/api/interfaces/IUserRoutes.ts
+++ src/api/interfaces/IUserRoutes.ts
@@ -1,7 +1,7 @@
 import { type Router } from 'express'
 import { type IUserController } from './IUserController'
 
-export interface IUserRutes {
+export interface IUserRoutes {
   readonly routes: Router
   readonly userController: IUserController
 }
