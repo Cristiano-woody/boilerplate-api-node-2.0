c81c289e6521c72cecc5a3e6aafeaa4433afecc5
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-21 20:09:19 -0300

    refactor: creating class Server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
-import { app } from './api/app'
+import App from './api/App'
 import * as dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
+import { type IServer } from './api/interfaces/ IServer'
 
 dotenv.config()
 
-const portServer = process.env.PORT_API
+class Server implements IServer {
+  readonly prisma: PrismaClient
+  readonly portServer: string | undefined
+  readonly app: App
+  constructor (portserver: string | undefined) {
+    this.prisma = new PrismaClient()
+    this.portServer = portserver
+    this.app = new App()
+  }
 
-app.listen(portServer, () => {
-  console.log(`\n Aplicação iniciada na porta: ${portServer as string} `)
-})
+  async start (): Promise<void> {
+    await this.prisma.$connect()
+    this.app.api.listen(this.portServer, () => {
+      console.log(`\n 🔥 Aplicação iniciada na porta: ${this.portServer as string} 🔥`)
+    })
+  }
+}
+
+const server = new Server(process.env.PORT_API)
+void server.start()
