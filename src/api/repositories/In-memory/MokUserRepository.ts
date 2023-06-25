5e12b8cf4318967a6ea8df9d447ad49fad13b20e
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-25 15:52:24 -0300

    refactor: modified methods on MokUserRepository

--- src/api/repositories/In-memory/MokUserRepository.ts
+++ src/api/repositories/In-memory/MokUserRepository.ts
@@ -1,31 +1,50 @@
 
 import type UserEntity from '../../entities/UserEntity'
-import { type IUserRepository } from '../../interfaces/IUserRepository'
+// import { type IUserRepository } from '../../interfaces/IUserRepository'
 
-class MokUserRepository implements IUserRepository {
-  async create (user: UserEntity): Promise<void> {
-    console.log('creating user')
+class MokUserRepository {
+  public users: UserEntity[]
+
+  constructor () {
+    this.users = []
+  }
+
+  async create (user: UserEntity): Promise<UserEntity> {
+    this.users.push(user)
+    return user
   }
 
   async getAll (): Promise<UserEntity[] | null> {
-    const AllUsers = [{ name: 'user1', email: 'user@gmail.com', id: 'www' }, { name: 'user1', email: 'user@gmail.com', id: 'www' }]
-    return AllUsers
+    return this.users
   }
 
   async getUser (id: string): Promise<UserEntity | null> {
-    const User = { name: 'user1', email: 'user@gmail.com', id: 'www' }
-    return User
+    for (let i = 0; i <= this.users.length; i++) {
+      const idUser = this.users[i].id
+      if (idUser === id) {
+        return this.users[i]
+      }
+    }
+    return null
+  }
+
+  async getUserByEmail (email: string): Promise<UserEntity | null> {
+    for (let i = 0; i <= this.users.length; i++) {
+      const idUser = this.users[i].email
+      if (idUser === email) {
+        return this.users[i]
+      }
+    }
+    return null
   }
 
   async update (body: UserEntity): Promise<UserEntity | null> {
     const User = { name: 'user1', email: 'user@gmail.com', id: 'www' }
-    console.log('atualizou')
     return User
   }
 
   async delete (id: string): Promise<UserEntity | null> {
     const User = { name: 'user1', email: 'user@gmail.com', id: 'www' }
-    console.log('deletou')
     return User
   }
 }
