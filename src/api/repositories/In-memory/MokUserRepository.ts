a5317b83c86986de6fa0f919336c4d1418035fed
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-29 23:03:12 -0300

    refactor: modified method on  user mok repository

--- src/api/repositories/In-memory/MokUserRepository.ts
+++ src/api/repositories/In-memory/MokUserRepository.ts
@@ -1,8 +1,8 @@
 
-import type UserEntity from '../../entities/UserEntity'
-// import { type IUserRepository } from '../../interfaces/IUserRepository'
+import UserEntity from '../../entities/UserEntity'
+import { type IUserRepository } from '../../interfaces/IUserRepository'
 
-class MokUserRepository {
+class MokUserRepository implements IUserRepository {
   public users: UserEntity[]
 
   constructor () {
@@ -10,40 +10,39 @@ class MokUserRepository {
   }
 
   async create (user: UserEntity): Promise<UserEntity> {
-    this.users.push(user)
-    return user
+    const newUser = new UserEntity(user)
+    this.users.push(newUser)
+    return newUser
   }
 
-  async getAll (): Promise<UserEntity[] | null> {
+  async getAll (): Promise<UserEntity[]> {
     return this.users
   }
 
-  async getUser (id: string): Promise<UserEntity | null> {
+  async getUser (id: string): Promise<UserEntity | undefined> {
     for (let i = 0; i <= this.users.length; i++) {
       const idUser = this.users[i].id
       if (idUser === id) {
         return this.users[i]
       }
     }
-    return null
   }
 
-  async getUserByEmail (email: string): Promise<UserEntity | null> {
+  async getUserByEmail (email: string): Promise<UserEntity | undefined> {
     for (let i = 0; i <= this.users.length; i++) {
       const idUser = this.users[i].email
       if (idUser === email) {
         return this.users[i]
       }
     }
-    return null
   }
 
-  async update (body: UserEntity): Promise<UserEntity | null> {
-    return null
+  async update (body: UserEntity): Promise<UserEntity | undefined> {
+    return undefined
   }
 
-  async delete (id: string): Promise<UserEntity | null> {
-    return null
+  async delete (id: string): Promise<UserEntity | undefined> {
+    return undefined
   }
 }
 
