0f66ad8b48b3f370dcb1983788db79deb008c8e5
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-30 20:26:42 -0300

    refactor: including user validator in index

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,14 @@ import UserRepository from './repositories/UserRepository'
 import UserService from './services/UserService'
 import UserController from './controllers/UserController'
 import UserRoutes from './routes/UserRoutes'
+import UserValidator from './validators/UserValidator'
 // import MokUserRepository from './api/repositories/MokUserRepository'
 
 // instanciando as classes e injetando dependencias
 const CreateUserRepository = new UserRepository()
 // const CreateMokUserRepository = new MokUserRepository()
-const CreateUserService = new UserService(CreateUserRepository)
+const userValidator = new UserValidator()
+const CreateUserService = new UserService(CreateUserRepository, userValidator)
 const CreateUserController = new UserController(CreateUserService)
 const CreateUserRoutes = new UserRoutes(CreateUserController)
 
