a9c9d262e3d5955250916627023c9d7a6c606ac7
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-30 20:30:43 -0300

    refactor: crianting tests to user service

--- src/api/services/UserService.spec.ts
+++ src/api/services/UserService.spec.ts
@@ -1,14 +1,22 @@
 import UserService from './UserService'
 import MokUserRepository from '../repositories/In-memory/MokUserRepository'
 import UserEntity from '../entities/UserEntity'
+import UserValidator from '../validators/UserValidator'
 
 describe('User Service', () => {
   const repository = new MokUserRepository()
-  const userService = new UserService(repository)
+  const userValidator = new UserValidator()
+  const userService = new UserService(repository, userValidator)
 
   it('shold be able to create a user', async () => {
-    const user = await userService.create({ name: 'John', email: 'john doe@gmail.com' })
-    expect(user).toHaveProperty('id')
-    expect(user).toBeInstanceOf(UserEntity)
+    const user = new UserEntity({ name: 'John', email: 'johndoe@gmail.com' })
+    const newUser = await userService.create(user)
+    expect(newUser).toHaveProperty('id')
+    expect(newUser).toBeInstanceOf(UserEntity)
+  })
+
+  it('shold not be able to create a user with invalid email', async () => {
+    const user = new UserEntity({ name: 'John', email: 'johnd oe@gmail.com' })
+    await expect(userService.create(user)).rejects.toEqual(new Error('Email is not valid'))
   })
 })
