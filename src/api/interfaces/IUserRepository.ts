8ad2c43ff5825763f574f0334ae2aeb62e7f8a44
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-01 23:28:31 -0300

    refactor: modified return in IUserRepository

--- src/api/interfaces/IUserRepository.ts
+++ src/api/interfaces/IUserRepository.ts
@@ -1,10 +1,10 @@
 import type UserEntity from '../entities/UserEntity'
 
 export interface IUserRepository {
-  create: (user: UserEntity) => Promise<UserEntity | undefined>
+  create: (user: UserEntity) => Promise<void>
   getUser: (id: string) => Promise<UserEntity | undefined>
   getUserByEmail: (email: string) => Promise<UserEntity | undefined>
   getAll: () => Promise<UserEntity[]>
-  update: (body: UserEntity) => Promise<UserEntity | undefined>
-  delete: (id: string) => Promise<UserEntity | undefined>
+  update: (body: UserEntity) => Promise<void>
+  delete: (id: string) => Promise<void>
 }
