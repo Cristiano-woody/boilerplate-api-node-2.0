b6de36074031c227045caff5b0a0c43b7a9efde9
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-03 11:37:43 -0300

    refactor: modifieding and creating new tests

--- src/api/services/UserService.spec.ts
+++ src/api/services/UserService.spec.ts
@@ -8,13 +8,25 @@ describe('User Service', () => {
   const userValidator = new UserValidator()
   const userService = new UserService(repository, userValidator)
 
+  // method createUser
   it('shold be able to create a user', async () => {
     const user = new UserEntity({ name: 'John', email: 'johnoe@gmail.com' })
-    await expect(userService.create(user)).resolves.not.toThrow(new Error('Email is not valid'))
+    await expect(userService.create(user)).resolves.not.toThrow()
   })
-
   it('shold not be able to create a user with invalid email', async () => {
     const user = new UserEntity({ name: 'John', email: 'johnd oe@gmail.com' })
     await expect(userService.create(user)).rejects.toEqual(new Error('Email is not valid'))
+  })
+  // method get User By Id
+  it('shold be able to get a user by id', async () => {
+    const newUser = new UserEntity({ name: 'John', email: 'eujohnd@gmail.com' })
+    await userService.create(newUser)
+    if (newUser.id !== undefined) {
+      const user = await userService.getUser(newUser.id)
+      expect(user).toBeInstanceOf(UserEntity)
+    }
+  })
+  it('shold not be able to get a user by id', async () => {
+    await expect(userService.getUser('iderror')).rejects.toEqual(new Error('User iderror does not exist'))
   })
 })
