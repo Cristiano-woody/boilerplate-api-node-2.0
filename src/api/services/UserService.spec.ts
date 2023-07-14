84911186d3feed33e95a733e2db3ca0ce90a911a
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-07-14 10:37:01 -0300

    test: modified structure

--- src/api/services/UserService.spec.ts
+++ src/api/services/UserService.spec.ts
@@ -3,12 +3,11 @@ import MokUserRepository from '../repositories/In-memory/MokUserRepository'
 import UserEntity from '../entities/UserEntity'
 import UserValidator from '../validators/UserValidator'
 
-describe('User Service', () => {
+describe('Create User', () => {
   const repository = new MokUserRepository()
   const userValidator = new UserValidator()
   const userService = new UserService(repository, userValidator)
 
-  // method createUser
   it('shold be able to create a user', async () => {
     const user = new UserEntity({ name: 'John', email: 'johnoe@gmail.com' })
     await expect(userService.create(user)).resolves.not.toThrow()
@@ -17,7 +16,13 @@ describe('User Service', () => {
     const user = new UserEntity({ name: 'John', email: 'johnd oe@gmail.com' })
     await expect(userService.create(user)).rejects.toEqual(new Error('Email is not valid'))
   })
-  // method get User By Id
+})
+
+describe('get user by id', () => {
+  const repository = new MokUserRepository()
+  const userValidator = new UserValidator()
+  const userService = new UserService(repository, userValidator)
+
   it('shold be able to get a user by id', async () => {
     const newUser = new UserEntity({ name: 'John', email: 'eujohnd@gmail.com' })
     await userService.create(newUser)
@@ -29,7 +34,13 @@ describe('User Service', () => {
   it('shold not be able to get a user by id', async () => {
     await expect(userService.getUser('iderror')).rejects.toEqual(new Error('User iderror does not exist'))
   })
-  // get user by email
+})
+
+describe('get user by email', () => {
+  const repository = new MokUserRepository()
+  const userValidator = new UserValidator()
+  const userService = new UserService(repository, userValidator)
+
   it('shold be able to get a user by email', async () => {
     const newUser = new UserEntity({ name: 'John', email: 'meuemail@gmail.com' })
     await userService.create(newUser)
@@ -39,12 +50,15 @@ describe('User Service', () => {
   it('shol not be able to get a user with email inexisting', async () => {
     await expect(userService.getUserByEmail('emailqualquer@kkkkk.com')).rejects.toEqual(new Error('User emailqualquer@kkkkk.com does not exist'))
   })
-  // get all users
+})
+
+describe('get all users', () => {
+  const repository = new MokUserRepository()
+  const userValidator = new UserValidator()
+  const userService = new UserService(repository, userValidator)
+
   it('shold be able to a get all users', async () => {
     const allUsers = await userService.getAll()
     expect(Array.isArray(allUsers)).toBe(true)
-    if (allUsers !== undefined) {
-      expect(allUsers[0]).toBeInstanceOf(UserEntity)
-    }
   })
 })
